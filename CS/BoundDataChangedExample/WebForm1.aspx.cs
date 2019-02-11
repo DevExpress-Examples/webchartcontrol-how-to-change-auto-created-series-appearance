c532092fce0cc936843546708b1b592c9503c177
Kseniya Kuzmina <kseniya.kuzmina@devexpress.com>
2019-02-11 16:56:56 +0300

    Update WebForm1.aspx.cs

--- CS/BoundDataChangedExample/WebForm1.aspx.cs
+++ CS/BoundDataChangedExample/WebForm1.aspx.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 using System.Data;
+using System.Linq;
 using DevExpress.XtraCharts;
 using DevExpress.XtraCharts.Web;
 
